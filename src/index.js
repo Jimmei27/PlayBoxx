f700a52bc0309e37895fc38f52bb3155960d0b2b
J Mei <Jmei@JMei-MacBook-Pro-7.local>
2020-05-01 18:26:03 -0400

    tweaks

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/gameBoard.jsx";
 import "./component/styles/styles.css";
+import { BrowserRouter as Router } from ‘react-router-dom’;
 
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>< App /></Router>, 
+    document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
