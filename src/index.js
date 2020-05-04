7d862afe7aa610db0c8ef786d5e33b3eaa116575
J Mei <Jmei@JMei-MacBook-Pro-7.local>
2020-05-04 01:19:28 -0400

    test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./component/gameBoard.jsx";
 import "./component/styles/styles.css";
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
-// ReactDOM.render(<HashRouter basename={process.env.PUBLIC_URL}>< App /></HashRouter>, 
-//     document.getElementById("root"));
+ReactDOM.render(<Router basename={process.env.PUBLIC_URL}>
+    < App />
+    </Router>, document.getElementById("root"));
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
